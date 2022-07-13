9725bd6235193ee37b250995cf0ed07e08bf8e2e
IvanSlatjukhin <slatjukhin@mail.ru>
2022-07-13 23:39:06 +0300

    Avia

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 10_000;
+        int percent = 20;
+        int bonus = amount / percent;
+
+        System.out.println("Иван, привет! Благодарим вас за выбор нашей авиакомпании для вашего рейса. Ваш бонус составляет:"+ bonus +" miles");
+    }
+}
